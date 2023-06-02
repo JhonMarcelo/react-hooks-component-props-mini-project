3793ec2155a1c6507e3cd735c82f763d90917b17
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-02 15:29:10 -0400

    Complete Putting all together lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { minutesToRead } from "../utility";
 
 const Article = ({ date = "January 1, 1970", minutes, preview, title }) => {
   return (
     <article>
       <h3>{title}</h3>
       <small>{date}</small>
+      {minutesToRead(minutes)}
       <p>{preview}</p>
     </article>
   );
